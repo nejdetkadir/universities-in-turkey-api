2b7788e17824b345060554c20a0173489b29471c
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-07 19:03:19 +0300

    Edit devise route

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/api/*',
+    resource '*',
       headers: %w(Authorization),
       methods: :any,
       expose: %w(Authorization),
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  devise_for :users, module: "api/v1", defaults: { format: :json }
-
   # get '/profile', to: "profile#show"
 
   scope 'api/v1', module: 'api/v1' do
+    devise_for :users, defaults: { format: :json }
+
     resources :universities
     resources :colleges
     resources :faculties
