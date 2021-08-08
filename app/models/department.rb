d6540038911287106502c1d5bc5954e46b38900d
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-08 15:34:36 +0300

    Add has many association to models

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,5 @@
 class Department < ApplicationRecord
   belongs_to :owner, polymorphic: true
 
-  validates_presence_of [:name, :owner_type, :owner_id, :years, 
-  :point_type, :quota, :quota_of_top_student_of_the_school, 
-  :special_conditions, :order_of_success_of_last_year, :base_score_of_last_year]
+  validates_presence_of [:name, :owner_type, :owner_id]
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,4 +1,7 @@
 class University < ApplicationRecord
   validates_uniqueness_of :name
   validates_presence_of :name
+
+  has_many :colleges
+  has_many :faculties
 end
