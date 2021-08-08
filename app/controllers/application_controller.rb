bdac7aa963777483a0573e6f6d69f403abd21436
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-08 16:28:48 +0300

    Create error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
   respond_to :json
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def check_admin
     raise ActionController::RoutingError.new('You are not authorized as an admin') unless current_user.admin?
+  end
+
+  def not_found
+    respond_with '{"error": "not_found"}', status: :not_found
   end
 end
